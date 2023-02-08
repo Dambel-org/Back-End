9b12b01696ff83ba8ec09bc2a13bce0aa74c8aa6
hossein5003 <hossein.borghei81@gmail.com>
2023-02-08 18:48:54 +0330

    [CREATE]trainee pre register and submit

--- Dambel/urls.py
+++ Dambel/urls.py
@@ -42,4 +42,5 @@ urlpatterns = [
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('account/', include('account.urls')),
+    path('gym/', include('gym.urls')),
 ]
--- account/views.py
+++ account/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import CreateAPIView
+from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import *
 
