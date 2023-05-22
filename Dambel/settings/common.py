98cdba50914cb21ee1bc2af899cf0d3432952598
amirhmn <amir.hosein6979@gmail.com>
2023-05-22 12:40:47 +0330

    [UPDATE] gym model and gym create serializer

--- Dambel/settings/common.py
+++ Dambel/settings/common.py
@@ -122,7 +122,3 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
-SIMPLE_JWT = {
-    'AUTH_HEADER_TYPES': ('JWT',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-}
--- gym/models.py
+++ gym/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from account.models import GymOwner, Trainee, Trainer
+from account.models import GymOwner, Trainee, Trainer, PhoneNumber
 
 
 class Province(models.Model):
@@ -12,12 +12,21 @@ class City(models.Model):
     province = models.ForeignKey(Province, on_delete=models.CASCADE, related_name='city')
 
 
+class SportField(models.Model):
+    name = models.CharField(max_length=200)
+
+
 class Gym(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='gym')
-    address = models.TextField()
     gym_owner = models.ForeignKey(GymOwner, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    logo_image = models.ImageField(upload_to='gym/logo/')
+    background_image = models.ImageField(upload_to='gym/background/')
+    description = models.TextField()
+    sport_filed = models.ManyToManyField(SportField)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='gym')
+    contacts = models.TextField()
+    phone_number = models.OneToOneField(PhoneNumber, on_delete=models.CASCADE)
+    # TODO location field on map
 
 
 class TrainerPreRegistration(models.Model):
